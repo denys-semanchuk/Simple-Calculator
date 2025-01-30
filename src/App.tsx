2063d5098f9240db93262c4d440880eaf91d54b8
Denys Semanchuk <semanchukdenys@icloud.com>
2025-01-30 21:49:03 +0100

    replaced handleButtonClick function in a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Wrapper, ButtonBox, Screen, Button, ModeToggle, History, ThemeToggle, ErrorNotification } from 'components';
 import { btnValues, engineeringButtons } from "utils/btnValues";
-import { backspaceHandler, closeBracketHandler, commaClickHandler, equalsClickHandler, handleKeyboard, numClickHandler, openBracketHandler, percentClickHandler, resetClickHandler, signClickHandler } from "utils/handlers";
+import { handleKeyboard } from "utils/handlers";
 import { ErrorState } from "types/errTypes";
 import { useTheme } from "context/ThemeContext";
-import { CalcState, SyntheticButtonEvent } from "types/calcTypes";
+import { CalcState } from "types/calcTypes";
 import { useHistory } from "context/HistoryContext";
-import { angleUnitHandler, constantHandler, factorialHandler, logarithmHandler, powerHandler, rootHandler, trigonometricHandler } from "utils/engineerHandlers";
 import cs from 'classnames';
 import { getLastMode, getLastResult, saveLastMode } from "utils/storageHandlers";
+import { handleButtonClick } from "utils/handleButtonClick";
 
 const App = () => {
   const lastSession = getLastResult();
@@ -37,81 +37,6 @@ const App = () => {
     }));
   };
 
-  const handleButtonClick = (e: SyntheticButtonEvent, btn: string | number) => {
-    switch (btn) {
-      case "(":
-        openBracketHandler(calc, setCalc);
-        break;
-      case ")":
-        closeBracketHandler(calc, setCalc);
-        break;
-      case "C":
-        resetClickHandler(calc, setCalc);
-        break;
-      case "←":
-        backspaceHandler(calc, setCalc);
-        break;
-      case "%":
-        percentClickHandler(calc, setCalc);
-        break;
-      case "=":
-        equalsClickHandler(calc, setCalc, addToHistory);
-        break;
-      case "/":
-      case "X":
-      case "-":
-      case "+":
-        signClickHandler(e, calc, setCalc);
-        break;
-      case ".":
-        commaClickHandler(e, calc, setCalc);
-        break;
-
-      case "sin":
-      case "cos":
-      case "tan":
-        trigonometricHandler(btn, calc, setCalc);
-        break;
-
-      case "log":
-      case "ln":
-        logarithmHandler(btn, calc, setCalc);
-        break;
-
-      case "x²":
-        powerHandler(2, calc, setCalc);
-        break;
-
-      case "√":
-        rootHandler(calc, setCalc);
-        break;
-
-      case "^":
-        powerHandler(calc.num, calc, setCalc);
-        break;
-
-      case "π":
-        constantHandler(Math.PI, calc, setCalc);
-        break;
-
-      case "e":
-        constantHandler(Math.E, calc, setCalc);
-        break;
-
-      case "!":
-        factorialHandler(calc, setCalc);
-        break;
-
-      case "rad":
-      case "deg":
-        angleUnitHandler(btn, calc, setCalc);
-        break;
-      default:
-        numClickHandler(e, calc, setCalc, setErr);
-        break;
-    }
-  }
-
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => handleKeyboard(e, calc, setCalc, setErr, addToHistory);
     window.addEventListener('keydown', handleKeyDown);
@@ -138,7 +63,7 @@ const App = () => {
                     "col4-8": btn === "=" && calc.mode === 'engineering',
                   })}
                   value={btn}
-                  onClick={(e) => handleButtonClick(e, btn)}
+                  onClick={(e) => handleButtonClick(calc, setCalc, addToHistory, setErr)(e, btn)}
                 />
               );
             })}
---
+++ src/utils/handleButtonClick.ts
@@ -0,0 +1,105 @@
+import { CalcState, SyntheticButtonEvent } from "types/calcTypes";
+import {
+  backspaceHandler,
+  closeBracketHandler,
+  commaClickHandler,
+  equalsClickHandler,
+  numClickHandler,
+  openBracketHandler,
+  percentClickHandler,
+  resetClickHandler,
+  signClickHandler,
+} from "./handlers";
+import {
+  angleUnitHandler,
+  constantHandler,
+  factorialHandler,
+  logarithmHandler,
+  powerHandler,
+  rootHandler,
+  trigonometricHandler,
+} from "./engineerHandlers";
+import { HistoryItem } from "types/historyTypes";
+import { ErrorState } from "types/errTypes";
+
+export const handleButtonClick = (
+  calc: CalcState,
+  setCalc: React.Dispatch<React.SetStateAction<CalcState>>,
+  addToHistory: (item: HistoryItem) => void,
+  setErr: React.Dispatch<React.SetStateAction<ErrorState>>
+) => {
+  return (e: SyntheticButtonEvent, btn: string | number) => {
+    switch (btn) {
+      case "(":
+        openBracketHandler(calc, setCalc);
+        break;
+      case ")":
+        closeBracketHandler(calc, setCalc);
+        break;
+      case "C":
+        resetClickHandler(calc, setCalc);
+        break;
+      case "←":
+        backspaceHandler(calc, setCalc);
+        break;
+      case "%":
+        percentClickHandler(calc, setCalc);
+        break;
+      case "=":
+        equalsClickHandler(calc, setCalc, addToHistory);
+        break;
+      case "/":
+      case "X":
+      case "-":
+      case "+":
+        signClickHandler(e, calc, setCalc);
+        break;
+      case ".":
+        commaClickHandler(e, calc, setCalc);
+        break;
+
+      case "sin":
+      case "cos":
+      case "tan":
+        trigonometricHandler(btn, calc, setCalc);
+        break;
+
+      case "log":
+      case "ln":
+        logarithmHandler(btn, calc, setCalc);
+        break;
+
+      case "x²":
+        powerHandler(2, calc, setCalc);
+        break;
+
+      case "√":
+        rootHandler(calc, setCalc);
+        break;
+
+      case "^":
+        powerHandler(calc.num, calc, setCalc);
+        break;
+
+      case "π":
+        constantHandler(Math.PI, calc, setCalc);
+        break;
+
+      case "e":
+        constantHandler(Math.E, calc, setCalc);
+        break;
+
+      case "!":
+        factorialHandler(calc, setCalc);
+        break;
+
+      case "rad":
+      case "deg":
+        angleUnitHandler(btn, calc, setCalc);
+        break;
+      default:
+        numClickHandler(e, calc, setCalc, setErr);
+        break;
+    }
+  };
+};
